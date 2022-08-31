b067e4944c505e14b2a19cfc54248b1149f10423
luca <lgorvin78@gmail.com>
2022-08-31 06:50:50 -0700

    Can now click address and open up in Google maps.

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './Business.css';
 
+
 class Business extends React.Component {
+  getGoogleUrl(address) {
+    return `https://www.google.com/maps/search/?api=1&query=${address}`
+  }
   render() {
     return (
       <div className="Business">
         <div className="image-container">
           <img src={this.props.business.imageSrc} alt=''/>
         </div>
-        <h2>{this.props.business.name}</h2>
+        
+        <a href={this.getGoogleUrl(this.props.business.address)}><h2>{this.props.business.name}</h2></a>
         <div className="Business-information">
           <div className="Business-address">
-            <p>{this.props.business.address}</p>
+            <a href={this.getGoogleUrl(this.props.business.address)}><p>{this.props.business.address}</p></a>
             <p>{this.props.business.city}</p>
             <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
           </div>
